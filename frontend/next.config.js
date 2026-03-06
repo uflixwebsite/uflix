f338cc958ece59113ad39ed5ca8aeb567d61aed3
Aimans30 <aiman.singh30@gmail.com>
2026-03-06 13:11:30 +0530

    Fixed images issue on home page

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -15,6 +15,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
